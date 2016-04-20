3b03cceb67187adf4c640633241dd7432d46765c
pickledyamsman <pickledyamsman@gmail.com>
2016-04-20 15:50:12 -0400

    Done.

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -7,4 +7,8 @@ class GithubRepo
     @url = hash["html_url"]
   end
 
+  def create_repo(name)
+    response = Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    redirect_to '/'
+  end
 end
